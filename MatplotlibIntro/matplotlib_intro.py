70b368b8bf38fc992714c45d90352ecf19fbfe8b
Gil Parnon <gparnon@pdx.edu>
2021-01-28 12:49:14 -0800

    Finished lab 4

--- MatplotlibIntro/matplotlib_intro.py
+++ MatplotlibIntro/matplotlib_intro.py
@@ -4,7 +4,8 @@
 <Class>
 <Date>
 """
-
+import numpy as np
+from matplotlib import pyplot as plt
 
 # Problem 1
 def var_of_means(n):
@@ -18,14 +19,21 @@ def var_of_means(n):
     Returns:
         (float) The variance of the means of each row.
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+    A = np.random.normal(size=(n,n))
+    m = A.mean(axis=1)
+    return np.var(m)
+   
 
 def prob1():
     """Create an array of the results of var_of_means() with inputs
     n = 100, 200, ..., 1000. Plot and show the resulting array.
     """
-    raise NotImplementedError("Problem 1 Incomplete")
-
+    n = 100*np.linspace(1,10,10)
+    x = [0]*len(n)
+    for i in range(len(n)):
+        x[i] = var_of_means(int(n[i]))
+    plt.plot(n,x)
+    return x
 
 # Problem 2
 def prob2():
@@ -33,7 +41,21 @@ def prob2():
     [-2pi, 2pi]. Make sure the domain is refined enough to produce a figure
     with good resolution.
     """
-    raise NotImplementedError("Problem 2 Incomplete")
+    pi = np.pi
+    x = np.linspace(-2*pi,2*pi,100)
+    print("Sin(x)")
+    a = np.sin(x)
+    b = np.cos(x)
+    c = np.arctan(x)
+    print("Sin(x)")
+    plt.plot(x,a)
+    plt.show()
+    print("Cos(x)")
+    plt.plot(x,b)
+    plt.show()
+    print("ArcTan(x)")
+    plt.plot(x,c)
+    plt.show()
 
 
 # Problem 3
@@ -44,7 +66,15 @@ def prob3():
         3. Set the range of the x-axis to [-2,6] and the range of the
            y-axis to [-6,6].
     """
-    raise NotImplementedError("Problem 3 Incomplete")
+    x1 = np.linspace(-2,1,50,endpoint=False)
+    x2 = np.flip(np.linspace(6,1,100,endpoint=False))
+    f1 = 1/(x1-1)
+    f2 = 1/(x2-1)
+    plt.plot(x1,f1,'m--', linewidth = 4)
+    plt.plot(x2,f2, 'm--', linewidth = 4)
+    plt.xlim(-2,6)
+    plt.ylim(-6,6)
+    plt.show()
 
 
 # Problem 4
@@ -61,21 +91,20 @@ def prob4():
              2sin(x): blue dashed line.
             2sin(2x): magenta dotted line.
     """
-    raise NotImplementedError("Problem 4 Incomplete")
-
-
-# Problem 5
-def prob5():
-    """Visualize the data in FARS.npy. Use np.load() to load the data, then
-    create a single figure with two subplots:
-        1. A scatter plot of longitudes against latitudes. Because of the
-            large number of data points, use black pixel markers (use "k,"
-            as the third argument to plt.plot()). Label both axes.
-        2. A histogram of the hours of the day, with one bin per hour.
-            Label and set the limits of the x-axis.
-    """
-    raise NotImplementedError("Problem 5 Incomplete")
-
+    x = np.linspace(0,2*np.pi)
+    fig, axs = plt.subplots(2,2)
+    axs[0,0].plot(x,np.sin(x),'g-')
+    axs[0,1].plot(x,np.sin(2*x),'r--')
+    axs[1,0].plot(x,2*np.sin(x),'b--')
+    axs[1,1].plot(x,2*np.sin(2*x),'m:')
+    plt.axis([0,2*np.pi,-2,2])
+    axs[0,0].set_title('Sin(x)')
+    axs[0,1].set_title('Sin(2x)')
+    axs[1,0].set_title('2Sin(x)')
+    axs[1,1].set_title('2Sin(2x)')
+    fig.suptitle('Various Sin functions')
+    plt.show()
+    
 
 # Problem 6
 def prob6():
@@ -88,4 +117,19 @@ def prob6():
         3. Choose a non-default color scheme.
         4. Add a colorbar to each subplot.
     """
-    raise NotImplementedError("Problem 6 Incomplete")
+    x = np.linspace(-2*np.pi,2*np.pi) 
+    y = x.copy()
+    X,Y = np.meshgrid(x,y)
+    Z = np.sin(X)*np.sin(Y)/(X*Y)
+    plt.subplot(121,aspect = 'equal')
+    plt.pcolormesh(X,Y,Z,cmap="magma")
+    plt.colorbar()
+    plt.xlim(-2*np.pi,2*np.pi)
+    plt.ylim(-2*np.pi,2*np.pi)
+
+    plt.subplot(122,aspect = 'equal')
+    plt.contour(X,Y,Z,10,cmap = "coolwarm")
+    plt.xlim(-2*np.pi,2*np.pi)
+    plt.ylim(-2*np.pi,2*np.pi)
+    plt.colorbar()
+    return
